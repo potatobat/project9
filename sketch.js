671796f473bd8762b27071134b47eda5297d2491
potatobat <86846275+potatobat@users.noreply.github.com>
2021-07-02 10:09:44 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var box
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(200,200,30,30)
+
+}
+
+function draw() 
+{
+
+
+  if(keyIsDown(RIGHT_ARROW))
+  {
+    background("red")
+  }
+  if(keyIsDown(LEFT_ARROW))
+  {
+    background("blue")
+  }
+  if(keyIsDown(UP_ARROW))
+  {
+    background("purple")
+  }
+  if(keyIsDown(DOWN_ARROW))
+  {
+    background("yellow")
+  }
+drawSprites()
+}
+
+
+
+
